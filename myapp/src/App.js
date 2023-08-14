00105e17338db70347021f1ba28398ca64dab5d8
Selvi P <selvi.ps.cse@gmail.com>
2023-08-14 14:00:29 +0530

    plz commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World!</h1>
+        <h2>React Basics</h2>
         <p>Development Started</p>
       </header>
     </div>
